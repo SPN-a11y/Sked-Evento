6332ead392d8ed74e22ddf5e9efb5c9df3b1d08e
SPN <84264659+SPN-a11y@users.noreply.github.com>
2021-05-25 20:44:17 +0530

    Update settings.py

--- skedevento/settings.py
+++ skedevento/settings.py
@@ -19,14 +19,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')
-DEBUG = config('DEBUG', default=False, cast=bool)
-DATABASES = {
-    'default': dj_database_url.config(
-        default=config('DATABASE_URL')
-    )
-}
+# SECURITY WARNING: keep the secret key used in production secret
+import os 
+    SECRET_KEY = os.getenv('SECRET_KEY', 'Optional default value')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
