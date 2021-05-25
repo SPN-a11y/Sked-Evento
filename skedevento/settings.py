028a1e4c5dfe53d08b8babc383ac35c5509d152c
SPN <84264659+SPN-a11y@users.noreply.github.com>
2021-05-25 19:36:47 +0530

    Update settings.py

--- skedevento/settings.py
+++ skedevento/settings.py
@@ -120,6 +120,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = ['static']
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
