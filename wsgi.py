5a1f732af4f8c234761ca1482fa3068bf35b26ee
SPN <sradhapremnaick756@gmail.com>
2021-05-26 14:12:36 +0530

    wsgi

--- wsgi.py
+++ wsgi.py
@@ -2,7 +2,7 @@ import os
 from django.core.wsgi import get_wsgi_application
 from whitenoise.django import DjangoWhiteNoise
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "bootcamp.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Sked-Evento.settings")
 
 application = get_wsgi_application()
 application = DjangoWhiteNoise(application)
