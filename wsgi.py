2866d53ddc8dff325f0be8ae818b7cf1f4f5565f
SPN <84264659+SPN-a11y@users.noreply.github.com>
2021-05-26 17:08:55 +0530

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,8 +1,7 @@
 import os
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
+
 from django.core.wsgi import get_wsgi_application
-from whitenoise.django import DjangoWhiteNoise
+from dj_static import Cling
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Sked-Evento.settings")
-
-application = get_wsgi_application()
-application = DjangoWhiteNoise(application)
+application = Cling(get_wsgi_application())
